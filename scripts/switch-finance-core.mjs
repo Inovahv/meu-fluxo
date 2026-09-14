f56b8f2dad2e5b1ceb78f987cb8b10799cf7d207
Inovahv <vieirahenrique321@gmail.com>
2026-09-14 20:04:48 -0300

    chore: remover script temporario do nucleo financeiro

--- scripts/switch-finance-core.mjs
+++
@@ -1,5 +0,0 @@
-import fs from 'node:fs'
-const path='src/AppV3.tsx'
-let source=fs.readFileSync(path,'utf8')
-source=source.replaceAll("'./financeCore.mjs'", "'./financeCore'")
-fs.writeFileSync(path,source)
